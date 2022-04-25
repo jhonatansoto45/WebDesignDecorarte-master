9e4ce49d4f51e7c0ddf4f7c1b1db895e0d5f29e7
Jhonatan Soto <61027750+Jhonatansoto45@users.noreply.github.com>
2022-04-25 11:45:56 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WebDesignDecorarte
+Pagina de productos por una empresa colombiana de manualidades y decoraciones propias.
---
+++ js/conecctionWhatsapp.js
@@ -0,0 +1,35 @@
+function isMobile() {
+    if (sessionStorage.desktop)
+        return false;
+    else if (localStorage.mobile)
+        return true;
+    var mobile = ['iphone', 'ipad', 'android', 'blackberry', 'nokia', 'opera mini', 'windows mobile', 'windows phone', 'iemobile'];
+    for (var i in mobile)
+        if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0) return true;
+    return false;
+}
+
+const formulario = document.querySelector('#formulario');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://web.whatsapp.com/';
+const urlMobile = 'whatsapp://';
+const telefono = '+573117944435';
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>'
+    buttonSubmit.disabled = true
+    setTimeout(() => {
+        let nombre = document.querySelector('#nombre').value
+        let apellidos = document.querySelector('#apellidos').value
+        let email = document.querySelector('#email').value
+        let mensaje = 'send?phone=' + telefono + '&text=DecorArte Ibagué%0ANombre:%0A' + nombre + '%0AApellido:%0A' + apellidos + '%0ACorreo electrónico:%0A' + email + ''
+        if (isMobile()) {
+            window.open(urlMobile + mensaje, '_blank')
+        } else {
+            window.open(urlDesktop + mensaje, '_blank')
+        }
+        buttonSubmit.innerHTML = '<i class="fab fa-whatsapp"></i> Enviar WhatsApp'
+        buttonSubmit.disabled = false
+    }, 3000);
+});
